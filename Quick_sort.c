a5748f33322e5ec047bab65a7ab97da00df2498a
PANKAJ RANA <pankajrana2754@gmail.com>
2025-06-11 18:43:29 +0530

    Update Quick_sort.c

--- Quick_sort.c
+++ Quick_sort.c
@@ -1,48 +1,76 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-//partition function 
-int partition(int arr[] , int start  , int end )
+
+// Display function 
+void display(int arr[], int n);
+
+// Swap function 
+void swap(int *first, int *second);
+
+// Partition Function 
+int partition(int arr[], int start, int end);
+
+int main()
 {
-    int pivot , temp , index;
-    pivot = arr[end];
-    index = start -1;
-    for (int i = start; i < end; i++)//i will raverse the array provided 
+    int arr[] = {10, 4, 6, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Before Sorting :\n");
+    display(arr, n);
+
+    printf("\nAfter Sorting :\n");
+    quicksort(arr, 0, n - 1);
+    display(arr, n);
+
+    return 0;
+}
+
+//display function defination 
+void display(int arr[], int n)
+{
+    printf("The array is : \n");
+    for (int i = 0; i < n; i++)
     {
-    if(arr[i] < pivot )
-      index++;
+        printf("%d ", arr[i]);
     }
-    arr[index] = pivot;
+}
+
+//Swap function defination
+void swap(int *first, int *second)
+{
+    int temp;
+    temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+//Partition function defination
+int partition(int arr[], int start, int end)
+{
+    int index = start - 1;
+    int temp, pivot = arr[end];
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            index++;
+            // swap
+            swap(&arr[index], &arr[j]);
+        }
+    }
+    index++;
+    swap(&arr[index], &arr[end]);
+
     return index;
 }
 
-void quick_sort(int arr[] , int start , int end)
+//Quicksort function dfination
+void quicksort(int arr[], int st, int end)
 {
-    if(start < end){
-int pivot_index = partition(arr , start ,end);
-quick_sort(arr , start ,pivot_index -1);
-quick_sort(arr , pivot_index+1 , end);
-}
-}
-
-void display(int arr[] , int n)
-{
-    printf("The array is:\n");
-    for (int i = 0; i < n; i++)
+    int pi; // pivotindex
+    if (st < end)
     {
-        printf(" %d",arr[i]);
+        pi = partition(arr, st, end);
+        quicksort(arr, st, pi - 1);
+        quicksort(arr, pi + 1, end);
     }
-    
-}
-int main()
-{
-    int arr[] = {4, 3 ,2,1 ,5};
-    int n =sizeof(arr)/sizeof(arr[0]);
-    printf("Before sorting \n");
-    display(arr , n);
-     quick_sort(arr , 0 ,n-1);
-     printf("After sorting : \n");
-    display(arr , n);
-
-    return 0;
+    return;
 }
